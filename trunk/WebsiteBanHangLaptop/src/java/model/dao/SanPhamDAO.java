5890d59b3c99e041f4629b2c17ee69115b1c660b
vhkhang91@gmail.com <vhkhang91@gmail.com@e226fcfd-322c-08e8-959c-5f223e935104>
2012-09-29 06:41:06 +0000

    asd

--- trunk/WebsiteBanHangLaptop/src/java/model/dao/SanPhamDAO.java
+++ trunk/WebsiteBanHangLaptop/src/java/model/dao/SanPhamDAO.java
@@ -4,7 +4,11 @@
  */
 package model.dao;
 
+import java.util.List;
+import model.dao.util.HibernateUtil;
 import model.pojo.SanPham;
+import org.hibernate.Query;
+import org.hibernate.classic.Session;
 
 /**
  *
@@ -16,5 +20,21 @@ public class SanPhamDAO extends ObjectDAO<SanPham, String> {
     protected Class getPOJOClass() {
         return SanPham.class;
     }
-    
+    public List<SanPham> search(String timkiem){
+        List<SanPham> list =null;
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        String[] str = timkiem.split(",");
+        String hql = String.format(
+                "select obj from SanPham obj where (obj.tenSanPham like :tensp and obj.hangSanXuat.maHang like :hangsx)"
+                );
+        
+        Query query = session.createQuery(hql);
+        query.setParameter("tensp", "%"+str[0]+"%");
+        
+        query.setParameter("hangsx", "%"+str[1]+"%");
+        list = query.list();
+        session.close();
+        
+        return list;
+    }
 }
